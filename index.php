1de34066920cd9e434e60f676a3f36cb0e0b73b9
Norcy <chenyg32@gmail.com>
2018-05-05 18:19:11 +0800

    Git Push Auto Update

--- index.php
+++ index.php
@@ -45,56 +45,55 @@ class wechatCallbackapiTest
 
     public function responseMsg()
     {
-        // echo "HelloWorld";
-        //$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
         $postStr = file_get_contents('php://input');
-        if (!empty($postStr))
+        if (empty($postStr))
         {
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-	    $keywords = explode(" ", $keyword);
-	    $objName = $keywords[0];
-	    $objType = "m";
+        	echo "EmptyPost";
+            exit;
+        }
+
+        $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+        $fromUsername = $postObj->FromUserName;
+        $toUsername = $postObj->ToUserName;
+        $keyword = trim($postObj->Content);
+    	$keywords = explode(" ", $keyword);
+    	$objName = $keywords[0];
+    	$objType = "m";
+
 	    if (count($keywords) > 1)
 	    {
-		$objType = $keywords[1];
+			$objType = $keywords[1];
 	    }
 	    if ($fromUsername == "o_iMAj1gZOuQIR_yDK7Sz5nsFqnw")
 	    {
-		include_once "blogUpdater.php";
-                updateBlog($objType, $objName, "", "", "");
+			include_once "blogUpdater.php";
+			updateBlog($objType, $objName, "", "", "");
 	    }
 
-            $time = time();
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
+        $time = time();
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[%s]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>0</FuncFlag>
+                    </xml>";
 
-            $msgType = "text";
-            if($fromUsername != "o_iMAj1gZOuQIR_yDK7Sz5nsFqnw" || $keyword == "?" || $keyword == "？")
-            {
-                $contentStr = date("Y-m-d H:i:s",time());
-            }
-            else
-            {
-                $contentStr = "http://norcy.github.io/2013/03/01/%E9%82%A3%E4%BA%9B%E5%B9%B4%EF%BC%8C%E6%88%91%E7%9C%8B%E8%BF%87%E7%9A%84/";
-            }
-            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-            echo $resultStr;
+        $msgType = "text";
+        if ($fromUsername != "o_iMAj1gZOuQIR_yDK7Sz5nsFqnw" || 
+        	$keyword == "?" || 
+        	$keyword == "？")
+        {
+            $contentStr = date("Y-m-d H:i:s",time());
         }
         else
         {
-            echo "EmptyPost";
-            exit;
+            $contentStr = "http://norcy.github.io/2013/03/01/%E9%82%A3%E4%BA%9B%E5%B9%B4%EF%BC%8C%E6%88%91%E7%9C%8B%E8%BF%87%E7%9A%84/";
         }
+        
+        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+        echo $resultStr;
     }
 }
 ?>
